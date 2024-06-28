59ec48ace85c680ad194ed2a331429dfa7e7aa7d
lirh42 <undercurre@163.com>
2024-06-28 18:09:07 +0800

    fix(api): 🧩 删除模板多余代码

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,1 @@
-<template>
-  <div class="home card">
-    <img class="home-bg" src="@/assets/images/welcome.png" alt="welcome" />
-  </div>
-</template>
-
-<script setup lang="ts" name="home"></script>
-
-<style scoped lang="scss">
-@import "./index.scss";
-</style>
+<template><div>123f</div></template>
