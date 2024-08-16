80f4c2284e30743b4b036bb370ca6a57f29386c6
lirh42 <undercurre@163.com>
2024-08-16 16:23:11 +0800

    feat(api): 🚀 初步完成用户管理模块

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -2,15 +2,16 @@
   <div class="card" style="height: 100%">
     <el-table :data="tableData" style="width: 100%; height: 100%">
       <el-table-column fixed prop="username" label="Name" width="120" />
-      <el-table-column prop="email" label="Email" width="120" />
-      <el-table-column prop="phone" label="Phone" width="320" />
-      <el-table-column prop="created_at" label="CreateTime" width="600" />
-      <el-table-column fixed="updated_at" label="UpdateTime" min-width="120">
+      <el-table-column prop="email" label="Email" width="240" />
+      <el-table-column prop="phone" label="Phone" width="120" />
+      <el-table-column prop="created_at" label="CreateTime" width="320" />
+      <el-table-column prop="updated_at" label="UpdateTime" width="320" />
+      <!-- <el-table-column :fixed="false" prop="updated_at" label="ToDo" min-width="120">
         <template #default>
           <el-button link type="primary" size="small"> Detail </el-button>
           <el-button link type="primary" size="small">Edit</el-button>
         </template>
-      </el-table-column>
+      </el-table-column> -->
     </el-table>
   </div>
 </template>
@@ -18,13 +19,17 @@
 <script lang="ts" setup>
 import { User } from "@/api/interface/user";
 import { getUserList } from "@/api/modules/user";
-import { ref } from "vue";
+import { onBeforeMount, ref } from "vue";
+
+import { ElTable } from "element-plus";
 
 let tableData = ref<Array<User.Entity>>([]);
 
-const listRes = await getUserList();
+onBeforeMount(async () => {
+  const listRes = await getUserList();
 
-console.log(listRes.data);
+  console.log(listRes.data);
 
-tableData.value = listRes.data;
+  tableData.value = listRes.data;
+});
 </script>
